597b2631087f3171b89ded9ad459845d75b03ed4
Wendy-bear <w6363980@gmail.com>
2022-01-02 22:20:02 +0300

    +

--- src/components/cardapp.jsx
+++ src/components/cardapp.jsx
@@ -3,39 +3,45 @@ import wordbook from "./dictionary";
 import React, { useState } from "react";
 
 function Card({ english, russian, transcription, tags, id }) {
-  let [reverse, setReverse] = useState(true);
+  let [reverse, setReverse] = useState(false);
 
   let handleReverse = () => {
     setReverse(!reverse);
   };
 
-  if (reverse) {
-    return (
-      <div className="card-wrapper">
-        <div className="card-word">{english}</div>
-        <div className="card-transcription">{transcription}</div>
-        <button onClick={handleReverse} className="card-reverse">
-          Проверить
-        </button>
-      </div>
-    );
-  } else {
-    return (
-      <div className="card-wrapper">
-        <div className="card-word">{english}</div>
-        <div className="card-transcription">{transcription}</div>
-        <div className="card-translate">{russian}</div>;
-      </div>
-    );
-  }
-  //вот тут не понимаю как через тернарный оператор редактирование сделать
+  //через if рендеринг работает и кнопка убирается
+
+  //  if (!reverse) {
+  //    return (
+  //      <div className="card-wrapper">
+  //        <div className="card-word">{english}</div>
+  //        <div className="card-transcription">{transcription}</div>
+  //        <button onClick={handleReverse} className="card-reverse">
+  //          Проверить
+  //        </button>
+  //      </div>
+  //    );
+  //  } else {
+  //    return (
+  //      <div className="card-wrapper">
+  //        <div className="card-word">{english}</div>
+  //        <div className="card-transcription">{transcription}</div>
+  //        <div className="card-translate">{russian}</div>
+  //      </div>
+  //    );
+  //  }
+
+  // рендеринг по кнопке - получается немного не то, что нужно. Не понимаю, куда нужно див для рендеринга засунуть, чтобы кнопка убиралась
+
   return (
     <div className="card-wrapper">
       <div className="card-word">{english}</div>
       <div className="card-transcription">{transcription}</div>
-      <button className="card-reverse" onClick={handleReverse}>
+
+      <button onClick={handleReverse} className="card-reverse">
         Проверить
       </button>
+      {reverse ? <div className="card-translate">{russian}</div> : null}
     </div>
   );
 }
--- src/components/word.jsx
+++ src/components/word.jsx
@@ -1,18 +1,36 @@
 import "./word.css";
 import wordbook from "./dictionary";
+import React, { useState } from "react";
 
-function Word({ english, russian, transcription, tags, id, showSave }) {
+function Word({ english, russian, transcription, tags, id }) {
+  let [save, showSave] = useState(false);
+
+  let handleSave = () => {
+    showSave(!save);
+  };
+
+  let [cancel, showCancel] = useState(false);
+
+  let handleCancel = () => {
+    showCancel(!cancel);
+  };
+
+  // вход в режим просмотра доступен по функци на кнопке  handleSave
+  //возможно схема работы этой кнопки должна была быть по собственному стейту(
+
   return (
     <tr>
-      <td>{showSave ? <input defaultValue={english} /> : english}</td>
-      <td>{showSave ? <input defaultValue={russian} /> : russian}</td>
+      <td>{save ? <input defaultValue={english} /> : english}</td>
+      <td>{save ? <input defaultValue={russian} /> : russian}</td>
+      <td>{save ? <input defaultValue={transcription} /> : transcription}</td>
+      <td>{save ? <input defaultValue={tags} /> : tags}</td>
       <td>
-        {showSave ? <input defaultValue={transcription} /> : transcription}
-      </td>
-      <td>{showSave ? <input defaultValue={tags} /> : tags}</td>
-      <td>
-        {showSave ? <button className="save-btn"></button> : null}
-        <button className="edit-btn"></button>
+        {save ? <button className="save-btn"></button> : null}
+        {save ? (
+          <button className="cancel-btn" onClick={handleSave}></button>
+        ) : null}
+
+        <button className="edit-btn" onClick={handleSave}></button>
         <button className="delete-btn"></button>
       </td>
     </tr>
