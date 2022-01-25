c3c6050fd0b23785f968f891219ba44f36e836ad
Wendy-bear <w6363980@gmail.com>
2022-01-25 19:06:27 +0300

    +

--- src/components/cardapp.jsx
+++ src/components/cardapp.jsx
@@ -15,11 +15,13 @@ function Card({
 
   const ref = useRef();
 
-  const reffocus = useRef();
-
   let handleReverse = () => {
     setReverse(!reverse);
   };
+
+  // не понимаю, как фокус на кнопку повесить
+  useEffect(() => ref.current.focus(), [id]);
+  console.log(ref);
 
   // перерисовка карточки при смене id
   useEffect(() => {
@@ -31,22 +33,13 @@ function Card({
   // нужно ли тут снятие обработчика кликов?
   // useEffect(() => ref.current.removeEventListener("click", changeLearn), [id]);
 
-  // не понимаю, как фокус на кнопку повесить
-  //useEffect(() => reffocus.current.focus(), [id]);
-  //console.log(reffocus);
-
   // выводит элемент после &&, если условие true)
   return (
     <div className="card-wrapper">
       <div className="card-word">{english}</div>
       <div className="card-transcription">{transcription}</div>
 
-      <button
-        onClick={handleReverse}
-        className="card-reverse"
-        ref={ref}
-        reffocus={reffocus}
-      >
+      <button onClick={handleReverse} className="card-reverse" ref={ref}>
         {reverse ? "Скрыть" : "Проверить"}
       </button>
       {reverse && <div className="card-translate">{russian}</div>}
